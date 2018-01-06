252c79761486ad3bbd0dfd0df1a711dd7b05705b
jgreen4919 <jon.green.440@gmail.com>
2018-01-06 15:37:49 -0500

    file path
    updated file path and cillizza example

--- ratio.R
+++ ratio.R
@@ -1,18 +1,19 @@
 # THE RATIO
 source("~/Desktop/rtweet/setup.R")
-source("~/Desktop/rtweet/ratiofunction.R")
+source("~/Desktop/rtweet/ratio/twitter_ratio/ratiofunction.R")
 
 library(rtweet)
 library(tidyverse)
 library(ggplot2)
 
 cillizza <- ratio(user = "CillizzaCNN", platform = "cnn.com")
-cillizza1 <- ggplot(cilizza, aes(x = ats, y = favorite_count))
+cillizza1 <- ggplot(cillizza, aes(x = ats, y = favorite_count))
 cillizza1 + geom_point() + geom_abline(slope = 1, intercept = 0)+
   xlab("Replies")+
   ylab("Faves")+
-  ggtitle("Chris Cillizza's Take Ratios (Last 9 Days)")+
+  ggtitle("Chris Cillizza's Take Ratios Since 1/4/17")+
   theme_bw()
+mean(cillizza$ratio1)
 
 lake <- ratio(user = "EliLake")
 lake1 <- ggplot(lake, aes(x = ats, y = favorite_count))
